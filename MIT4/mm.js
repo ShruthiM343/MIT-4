3b95ce758aff711aafa2486a5ffb2f68ba5451fd
ShruthiM343 <164155646+ShruthiM343@users.noreply.github.com>
2024-05-18 11:21:04 +0530

    Add files via upload

---
+++ MIT4/mm.js
@@ -0,0 +1,7 @@
+function submit(){
+    let name = document.getElementById('name').value;
+    let mailid= document.getElementById('email').value;
+    document.getElementById("n").innerText=name;
+    document.getElementById("em").innerText=mailid;
+    document.getElementById("del");
+}
